0c82576817a48275187463ffabd98452e8a9b9c8
kadyan2001 <116459752+kadyan2001@users.noreply.github.com>
2025-01-01 20:09:12 +0000

    datatype commit

---
+++ basics_01/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as new version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("Manish")
+
+let name="Manish"
+let age=23
+let isLoggedin=false
+
+// number => 2 to power 53
+// bigint
+// string=""
+// boolean => true/false
+// null
+// undefined
+// symbol => unnique
+
+
+// object non primitive datatype
+
+console.log(typeof null); //object datatype
+console.log(typeof undefined); //undefined
