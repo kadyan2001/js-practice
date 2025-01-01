13c82992f690ae558ff4b5c1faa0ae3c77190999
kadyan2001 <116459752+kadyan2001@users.noreply.github.com>
2025-01-01 20:19:19 +0000

    converion of datatype

---
+++ basics_01/conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber=Number(score);
+
+console.log(typeof valueInnumber);
+
+// "33" => 33
+// "33abc" => NaN
+
