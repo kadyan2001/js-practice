b80169ee86dbfd69687e77461bca7e84227d26ea
kadyan2001 <116459752+kadyan2001@users.noreply.github.com>
2025-01-09 19:05:48 +0000

    object2 commit

---
+++ basics_02/object2.js
@@ -0,0 +1,53 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "1342aaf"
+tinderUser.name = "Manish"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "man@gmail.com",
+
+    fullname : {
+        userFullname:{
+            firstname: "Mansih",
+            lastname : "Kadyan"
+        }
+    }
+}
+// console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1: "a", 2: "b" }
+const obj2 = {3: "a" , 4: "b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({} , obj1 , obj2)
+
+const obj3= {...obj1 , ...obj2}
+// console.log(obj3);
+
+const users=[
+    {
+        id: 1,
+        email : "user@gmail.com"
+    },
+    {
+        id: 1,
+      
+        email : "user@gmail.com"
+    }
+
+]
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(Object.hasOwnProperty('isLoggedIn'));
+
+
